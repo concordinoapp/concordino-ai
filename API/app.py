bb7bfdab4c6f7e7a82e6fa9e60af7779bd60cdcc
MATTEO DE VITA <matteo.de-vita@capgemini.com>
2023-03-25 10:50:35 +0100

    trian model v4

--- API/app.py
+++ API/app.py
@@ -15,7 +15,7 @@ UPLOADED_FILE_DIRECTORY = os.getcwd() + "/" + "__uploaded_files__/"
 if os.path.exists(UPLOADED_FILE_DIRECTORY) == False:
     os.makedirs(UPLOADED_FILE_DIRECTORY)
 
-cnn_ocr_model = tf.keras.models.load_model('./models/CNN-OCR-MODEL-SAVE-V2/')
+cnn_ocr_model = tf.keras.models.load_model('./models/CNN-MODEL-V4/')
 cnn_ocr_prediction_model = keras.models.Model(
     cnn_ocr_model.get_layer(name='image').input,
     cnn_ocr_model.get_layer(name='dense2').output
@@ -26,10 +26,9 @@ def ping():
     return "AI API running"
 
 #!!!!!!temp
-max_length = 50
+max_length = 32
 
-characters = [' ', '!', '"', '#', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '?', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
+characters = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '=', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', ']', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 char_to_num = layers.StringLookup(vocabulary=list(characters), mask_token=None)
 num_to_char = layers.StringLookup(vocabulary=char_to_num.get_vocabulary(), mask_token=None, invert=True)
 
@@ -44,8 +43,8 @@ def decode_cnn_ocr_prediction_model(pred):
         output_text.append(res)
     return output_text
 
-image_height = 32
-image_width = 128
+image_height = 64
+image_width = 256
 
 def encode_single_sample(image_path, label):
     # Read the image with tensorflow
